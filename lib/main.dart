69e7547586f4339167876b956930bf7b78b46e41
ronieo <romelzen50@gmail.com>
2021-11-28 14:41:49 +0900

    try to make setState and function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+
+class BallPage extends StatefulWidget {
+  const BallPage({key}) : super(key: key);
+
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+
+  int ballNum = 0;
+
+  void changeBallNum () {
+    setState(() {
+      ballNum = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        title: Text('Ask Me Everything'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            print('I got cliked!!');
+            changeBallNum();
+          },
+          child: Image.asset(
+              'images/ball$ballNum.png'
+          ),
+        ),
       ),
     );
+  }
+}
+
+
